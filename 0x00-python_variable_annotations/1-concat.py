16b3c9abf166d1502558d5e5bffa494870f1d40c
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-11 13:21:23 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,7 +5,7 @@ This is our modul
 
 
 def concat(str1: str, str2: str) -> str:
-  """
-  This is concat funct annotated
-  """
-  return str1 + str2
+    """
+    This is concat funct annotated
+    """
+    return str1 + str2
