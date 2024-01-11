ed38e789b275abeb71af2daf0d75bfb7740355dd
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-11 13:50:13 +0100

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from typing import List
 """
 This is our module
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     This is our sum_list
     """
