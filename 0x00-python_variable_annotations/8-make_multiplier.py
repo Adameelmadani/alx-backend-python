fa30e7ec291f7a5d3399bce538fcbf22da76234e
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-11 14:30:58 +0100

    Create 8-make_multiplier.py

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This is our module
+"""
+from typing import Callable
+"""
+This is our typing module
+"""
+
+    
+def make_multiplier(float: multiplier) -> Callable[[float], float]:
+    """
+    This is our multiplier func
+    """
+    def multiply(float: a) -> float:
+        """
+        This is multiply function callable one
+        """
+        f: float = a * multiplier
+        return f
+    return multiply
