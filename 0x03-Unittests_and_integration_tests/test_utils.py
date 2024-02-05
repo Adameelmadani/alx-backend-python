a09a6d85b1f627f7f36b2959f5bf8f9a25e6b516
Adam <adamelmadani0129@gmail.com>
2024-02-05 20:38:13 +0000

    1.task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,6 +23,16 @@ class TestAccessNestedMap(unittest.TestCase):
         """This function tests access_nested_map function"""
         self.assertEqual(access_nested_map(input["nm"], input["p"]), expected)
 
+    @parameterized.expand([
+        ({"nm": {}, "p": ("a",), "key": "\'a\'"}, 2),
+        ({"nm": {"a": 1}, "p": ("a", "b"), "key": "\'b\'"}, 2)
+        ])
+    def test_access_nested_map_exception(self, input, expected):
+        """This function tests access_ensted_map() if it raises exceptions"""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(input["nm"], input["p"])
+        self.assertEqual(str(e.exception), input["key"])
+
 
 if __name__ == "__main__":
     unittest.main()
