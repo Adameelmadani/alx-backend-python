944910e9e3d55c54620cf7b364d151ea88cc75d7
Adam <adamelmadani0129@gmail.com>
2024-02-06 19:27:37 +0000

    task

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -29,6 +29,17 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(test.org, org_json)
         get_mock.assert_called_once()
 
+    def test_public_repos_url(self):
+        """
+        Testing public_repos_url function
+        """
+        test = GithubOrgClient("url")
+        with patch("client.GithubOrgClient.org") as get_mock:
+            with patch("client.GithubOrgClient._public_repos_url") as get_m2:
+                get_mock.return_value = {"repos_url": "google"}
+                get_m2.return_value = get_mock.return_value["repos_url"]
+                self.assertEqual(test._public_repos_url, "google")
+
 
 if __name__ == "__main__":
     unittest.main()
